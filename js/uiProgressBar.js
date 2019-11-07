f64b1a5e2233f064e79cbff20f1aa2612df710b1
Jared Stanley <jared@jaredstanley.com>
2019-11-06 19:40:16 -0800

    working on adding tweening

--- js/uiProgressBar.js
+++ js/uiProgressBar.js
@@ -1,4 +1,5 @@
 import utils from './utils';
+import tweenFunctions from './tweenFunctions';
 let x=0;
 let y=0;
 let heightPercentage = 75; //0-100
@@ -14,6 +15,13 @@ let topMargin = 60;
 let itemsArr = [];
 let xpos = 0;
 let currentSection = 0;
+let highlight = {
+    needsUpdate:false,
+    iteration:0,
+    totalIterations:100,
+    x:0,y:0,w:0,h:0,
+    curH:0
+}
 //
 exports.init = function(){
     context = _App.context;
@@ -36,33 +44,40 @@ exports.update = function(){
     }
     // context.save();
     // context.globalAlpha=0.2;    
-    for (let i = 0; i < itemsArr.length; i++) {
-        let itm = itemsArr[i];
+    itemsArr.forEach(itm =>{
         context.fillStyle = itm.color;
         context.fillRect(xpos, itm.y, navItemWidth, itm.height); 
-    }
-    for (let itm in itemsArr) {
-        context.fillStyle = itm.color;
-        context.fillRect(xpos, itm.y, navItemWidth, itm.height); 
-    }
+    })
+
     // context.restore();
     let s = fontSizeOrig;
     if(itmHeight<35){s=fontSizeSm}
     addTextLabels(s);
+
+    if(highlight.needsUpdate){
+        updateHighlight();
+    }
+    context.fillStyle="#deadaf";
+    let curSec = sections-(currentSection+1);
+    context.fillRect(100,itemsArr[curSec].y-highlight.h, 10, highlight.h);
 }
 
 function buildNav(){
+    
     itemsArr = [];
     let defaultColor = "";
     itmHeight = ((_App.h*(heightPercentage*0.01))/sections)-margin;
     itmHeight = Math.max(itmHeight, itmMaxHeight);
     let c = utils.getColors();
+    // console.log(`heyyo ${currentSection}`);
+    
     for (let i = 0; i < sections; i++) {
         let sec = sections-currentSection;
         
-        if(i<sec)defaultColor=c.upcoming
-        else if(i>sec)defaultColor=c.visited
-        else defaultColor=c.brightGreen;
+        if(i<sec){defaultColor=c.upcoming;}
+        else if(i>sec){defaultColor=c.visited}
+        else {defaultColor=c.brightGreen;
+        }
         
         let itm = {
             color: defaultColor,
@@ -72,9 +87,37 @@ function buildNav(){
         }
         // console.log(itm.i, itm.y);
         
-        itemsArr.push(itm);   
+        itemsArr.push(itm); 
+        
     }   
+    highlight.needsUpdate=true;
+    highlight.h=0;
+    highlight.curH=0;
 }
+
+function updateHighlight(){
+    let hl = highlight;
+    console.log("updateHighlight");
+    // console.log(sections, c);
+   
+    hl.iteration++;  
+    if (hl.iteration < hl.totalIterations) {
+        hl.h = tweenFunctions.easeInOutQuart(hl.iteration, hl.curH, itmHeight, hl.totalIterations);
+        // hl.y = tweenFunctions.easeInOutQuart(hl.iteration, hl.curSpot.y, hl.tgt.y-hl.curSpot.y, hl.totalIterations);
+        hl.curH = hl.h;
+
+    }else{
+        hl.iteration=0;
+        hl.needsUpdate=false;
+    }
+
+    
+}
+
+
+
+
+
 function addTextLabels(s){
     context.fillStyle = utils.getColors().brightGreen;
     context.font = "300 "+s+"px Roboto";
